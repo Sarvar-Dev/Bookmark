c04b97770dc4dd91812bb412ae1323384df9044a
Sarvar-Dev <sarvarbekr1203@gmail.com>
2024-01-19 14:25:53 +0500

    FAQ section add js codes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ const elsTabsItem = document.querySelectorAll(".tabs__item");
 const elsTabLink = document.querySelectorAll(".js-tab-link");
 const elsTabsPanel = document.querySelectorAll('.tabpanels__item'); 
 
+const elsAccordingOpenToggler = document.querySelectorAll(".accordion__item-toggler");
+const elsAccordionItem = document.querySelectorAll(".accordion__item");
+
+function closeAccordionItems(){
+    elsAccordionItem.forEach(function(elAccordionItem){
+        elAccordionItem.classList.remove('accordion__item--open')
+    })
+}
+
 function deactivateTabItems(){
     elsTabsItem.forEach(function(elTabsItem){
         elTabsItem.classList.remove('tabs__item--active');
@@ -12,7 +21,6 @@ function deactivateTabPanels(){
         elTabsPanel.classList.remove('tabpanels__item--active');
     });     
 }   
-
 
 elsTabLink.forEach(function(elTabLink){
     elTabLink.addEventListener('click',function(evt){
@@ -35,3 +43,11 @@ elsTabLink.forEach(function(elTabLink){
         elTargetPanel.classList.add('tabpanels__item--active');
     });
 });
+
+elsAccordingOpenToggler.forEach(function(elAccordingOpenToggler){
+    elAccordingOpenToggler.addEventListener('click',function(){
+        closeAccordionItems();
+        // According open
+        elAccordingOpenToggler.closest(".accordion__item").classList.toggle('accordion__item--open');
+    })
+})
